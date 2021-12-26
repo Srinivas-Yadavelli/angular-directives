572a5fc070170c46bc400acec2256aabe7ac1859
Srinivas-Yadavelli <techseen9@gmail.com>
2021-12-27 00:23:03 +0530

    using @Hostbinding

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,9 +1,15 @@
-import { Directive, OnInit, Renderer2, ElementRef, HostListener } from '@angular/core';
+import { Directive,
+         OnInit,
+         Renderer2,
+         ElementRef,
+         HostListener,
+          HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') bgvColor : string = 'transparent';
 
   constructor(private eleRef : ElementRef, private renderer : Renderer2 ) { }
 //best example without messing up with DOM elements
@@ -12,10 +18,13 @@ export class BetterHighlightDirective implements OnInit {
  }
 
  @HostListener('mouseenter') mouseOver(eventData : Event){
- this.renderer.setStyle(this.eleRef.nativeElement, 'background-color', 'blue');
+ //this.renderer.setStyle(this.eleRef.nativeElement, 'background-color', 'blue');
+this.bgvColor= 'yellow';
  }
  @HostListener('mouseleave') mouseLeave(eventData : Event){
- this.renderer.setStyle(this.eleRef.nativeElement, 'background-color', 'red');
+// this.renderer.setStyle(this.eleRef.nativeElement, 'background-color', 'red');
+ this.bgvColor= 'green';
+
  }
 
 }
